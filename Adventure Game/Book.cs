fa4883405a822b576fd697210f37700be4305a9e
WibblyWall <161527301+WibblyWall@users.noreply.github.com>
2024-04-16 21:25:12 -0500

    Add files via upload

---
+++ Adventure Game/Book.cs
@@ -0,0 +1,40 @@
+﻿using Mid_Term_Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potion_Game
+{
+    class Book
+    {
+        public void Chapterone()
+        {
+            Print("\"Welcome to the potion makers guide book.\"");
+            Print("\"This book will teach you all you need to know to start making your own potions.\"");
+            Print("\"In this first chapter you will learn how to make the most basic potion.\"");
+            Print("\"A potion of healing.\"");
+            Print("\"For this potion you will need to following ingredients.\"");
+            Print("\"1.) Fresh Water\"");
+            Print("\"2.) Recovery Herb\"");
+            Print("\"3.) Tree Sap\"");
+            Print("\"Mix these up for 10 minutes then drain it.\"");
+            Print("\"You will have a perfectly good potion of healing.\"");
+            Print("\"Happy potion making!!!\"");
+            Print("You put the book away.");
+            Print("Press Any Key To Continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void Print(string text, int speed = 25)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                System.Threading.Thread.Sleep(speed);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Adventure Game/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mid_Term_Project
+{
+    public class Player
+    {
+        public string name = "";
+        public int gold = 5;
+        public int stamina = 10;
+        public int health = 5;
+        public int invitorySpace = 5;
+    }
+}
